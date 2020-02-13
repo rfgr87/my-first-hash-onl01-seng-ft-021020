b1a871e7fcb9601b5f75af0ed91093febdaaf8c5
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-13 16:37:43 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,16 +1,11 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  hash = {"name" => "Joe", "age" => "32"}
 end
 
 
+
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+ the_manifest = {"whale bone" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
